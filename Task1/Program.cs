7ef7b2d003bab2fd2c34ca487a24d0f22813b1e2
Maxim Kornilov <M.kornilov.flights@gmail.com>
2023-02-07 11:55:45 +0300

    нахождение наибольшей цифры в случайном числе из отрезка 10-99

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая выводит случайное
+// число из отрезка [10, 99] и показывает 
+ //наибольшую цифру числа. 
+// 25 => 5
+int num = new Random().Next(10, 100);
+System.Console.WriteLine(num);
+int firstNum = num / 10;
+int secondNum = num % 10;
+    if (firstNum > secondNum)
+    {
+        System.Console.WriteLine(firstNum);
+    }
+    else
+    {
+        System.Console.WriteLine(secondNum);
+    }
