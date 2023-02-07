bae4a46bd0180f4241469afa44a6f6af3ee3e3d6
Maxim Kornilov <M.kornilov.flights@gmail.com>
2023-02-07 15:25:19 +0300

    напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа

---
+++ Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая будет 
+//принимать на вход два числа и выводить,
+// является ли второе число кратным первому.
+// Если число 2 не кратно числу 1, то 
+//программа выводит остаток от деления.
+
+//34, 5 -> не кратно, остаток 4 
+//16, 4 -> кратно
+
+System.Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int result = num1 % num2;
+
+
+   if (result == 0)
+  {
+    System.Console.WriteLine("-> кратно");
+    }
+  else
+
+  {
+    
+    System.Console.WriteLine($"-> не кратно, остаток {result}");
+    
+}
